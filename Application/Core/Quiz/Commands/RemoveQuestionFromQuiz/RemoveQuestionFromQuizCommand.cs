f998c0102257a04c61f8c5ed42136785fe2bff56
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-04 00:41:05 +0200

    Adding command and handler for removing question for quiz

---
+++ Application/Core/Quiz/Commands/RemoveQuestionFromQuiz/RemoveQuestionFromQuizCommand.cs
@@ -0,0 +1,4 @@
+﻿using MediatR;
+
+namespace Application.Core.Quiz.Commands.RemoveQuestionFromQuiz;
+public sealed record RemoveQuestionFromQuizCommand(int QuizId, int QuestionId) : IRequest<Unit>;
---
+++ Application/Core/Quiz/Commands/RemoveQuestionFromQuiz/RemoveQuestionFromQuizHandler.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using MediatR;
+
+namespace Application.Core.Quiz.Commands.RemoveQuestionFromQuiz;
+internal sealed class RemoveQuestionFromQuizHandler : IRequestHandler<RemoveQuestionFromQuizCommand, Unit>
+{
+    private readonly IRepositoryManager _repository;
+
+    public RemoveQuestionFromQuizHandler(IRepositoryManager repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Unit> Handle(RemoveQuestionFromQuizCommand request, CancellationToken cancellationToken)
+    {
+
+        var question = await _repository.Question.GetQuestionById(request.QuestionId);
+
+        await _repository.SaveAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
