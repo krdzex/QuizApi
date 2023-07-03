a98b85dc347ecc1dd44c922e2aef83e492085fc2
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 14:17:40 +0200

    Adding entity configuration builder for question

---
+++ Repository/Configuration/QuestionConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Configuration;
+public class QuestionConfiguration : IEntityTypeConfiguration<Question>
+{
+    public void Configure(EntityTypeBuilder<Question> builder)
+    {
+        builder.HasKey(q => q.Id);
+        builder.Property(q => q.Text).IsRequired().HasMaxLength(1000);
+        builder.Property(q => q.Answer).IsRequired().HasMaxLength(1000);
+    }
+}
