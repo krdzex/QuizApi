1af4737f69e17d4a44058a8394c74a0100b95a82
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 15:25:55 +0200

    Adding empty get quizzes handler with repository manager just to test

---
+++ Application/Core/Quiz/Queries/GetQuizzes/GetQuizzesHandler.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using MediatR;
+
+namespace Application.Core.Quiz.Queries.GetQuizzes;
+internal sealed class GetQuizzesHandler : IRequestHandler<GetQuizzesQuery, Unit>
+{
+    private readonly IRepositoryManager _repository;
+
+    public GetQuizzesHandler(IRepositoryManager repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Unit> Handle(GetQuizzesQuery request, CancellationToken cancellationToken)
+    {
+        return Unit.Value;
+    }
+}
