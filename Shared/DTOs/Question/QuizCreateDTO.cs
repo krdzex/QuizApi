d119a90b1052107474b9c82476e1855ec243a823
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 16:55:35 +0200

    Make Dto for create quiz

---
+++ Shared/DTOs/Question/QuizCreateDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.DTOs.Question;
+public class QuizCreateDTO
+{
+    public string Name { get; set; }
+    public List<QuestionDTO> NewQuestions { get; set; }
+    public List<int> ExistingQuestionIds { get; set; }
+}
