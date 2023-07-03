8594701c22e2f17d9b53e8eddab8d4592f82c71d
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 13:50:43 +0200

    Adding reference from main project to Presentation layer

--- QuizApi/Configuration/DependencyInjection.cs
+++ QuizApi/Configuration/DependencyInjection.cs
@@ -1,6 +1,25 @@
-﻿namespace QuizApi.Configuration
+﻿using Microsoft.OpenApi.Models;
+
+namespace QuizApi.Configuration;
+public static class DependencyInjection
 {
-    public class DependencyInjection
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
+        services.AddControllers(config =>
+        {
+            config.RespectBrowserAcceptHeader = true;
+            config.ReturnHttpNotAcceptable = true;
+        }).AddXmlDataContractSerializerFormatters()
+          .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
+
+        services.AddSwaggerGen(s =>
+        {
+            s.SwaggerDoc("v1", new OpenApiInfo { Title = "QuizApi", Version = "v1" });
+        });
+
+        services.AddSwaggerGen();
+        services.AddEndpointsApiExplorer();
+
+        return services;
     }
 }
