f65bb262d8e39886ffd93aaacb3466d564bd5fa7
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 13:57:12 +0200

    Deleteing initial weatcher controller and adding our empty Question and Quiz controllers

---
+++ QuizApi.Presentation/Controllers/QuestionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizApi.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class QuestionController : ControllerBase
+{
+
+    public QuestionController()
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get(CancellationToken cancellationToken)
+    {
+        return Ok();
+    }
+}
---
+++ QuizApi.Presentation/Controllers/QuizController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizApi.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class QuizController : ControllerBase
+{
+
+    public QuizController()
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get(CancellationToken cancellationToken)
+    {
+        return Ok();
+    }
+}
