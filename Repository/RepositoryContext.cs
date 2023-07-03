2e7a149d096a2efc8dae20e6e72c224e76e9765f
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 14:15:45 +0200

    Create the RepositoryContext class which will be a middleware component for communication with the database

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<QuizQuestion> QuizQuestions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(RepositoryContext).Assembly);
+        }
+    }
+}
