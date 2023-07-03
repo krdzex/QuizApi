2ef0cf769fea0ecceab71ddbb33bf50afc2dff91
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 14:18:16 +0200

    Adding entity configuration builder for quiz

---
+++ Repository/Configuration/QuizConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Configuration;
+public class QuizConfiguration : IEntityTypeConfiguration<Quiz>
+{
+    public void Configure(EntityTypeBuilder<Quiz> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+    }
+}
