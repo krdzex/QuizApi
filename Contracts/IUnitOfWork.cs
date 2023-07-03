67fd98dfec564a2995e7bee206bee5104bf2ab53
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 16:47:08 +0200

    Made unit of work for saving changes to db

---
+++ Contracts/IUnitOfWork.cs
@@ -0,0 +1,5 @@
+﻿namespace Contracts;
+public interface IUnitOfWork
+{
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿namespace Repository
+{
+    internal sealed class UnitOfWork
+    {
+        private readonly RepositoryContext _context;
+
+        public UnitOfWork(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
