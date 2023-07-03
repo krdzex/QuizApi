00850b94cca1b3379a169f6caf79d31221791ef6
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 14:51:29 +0200

    Adding our initial migration to create all tables and columns

---
+++ QuizApi/Migrations/20230703125057_InitialMigration.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace QuizApi.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "questions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    text = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    answer = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_questions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "quizzes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_quizzes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "quiz_questions",
+                columns: table => new
+                {
+                    quiz_id = table.Column<int>(type: "integer", nullable: false),
+                    question_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_quiz_questions", x => new { x.quiz_id, x.question_id });
+                    table.ForeignKey(
+                        name: "fk_quiz_questions_questions_question_id",
+                        column: x => x.question_id,
+                        principalTable: "questions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_quiz_questions_quizzes_quiz_id",
+                        column: x => x.quiz_id,
+                        principalTable: "quizzes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_quiz_questions_question_id",
+                table: "quiz_questions",
+                column: "question_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "quiz_questions");
+
+            migrationBuilder.DropTable(
+                name: "questions");
+
+            migrationBuilder.DropTable(
+                name: "quizzes");
+        }
+    }
+}
