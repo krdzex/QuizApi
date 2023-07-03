d9034ca5311a396765a855ad031fd9fad7723415
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 19:41:17 +0200

    Finished handler and query for get questions

---
+++ Application/Core/Question/Queries/GetQuestions/GetQuestionsHandler.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using MediatR;
+using Shared.DTOs.Question;
+
+namespace Application.Core.Question.Queries.GetQuestions;
+internal sealed class GetQuestionsHandler : IRequestHandler<GetQuestionsQuery, IEnumerable<QuestionWithIdDTO>>
+{
+    private readonly IRepositoryManager _repository;
+
+    public GetQuestionsHandler(IRepositoryManager repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<QuestionWithIdDTO>> Handle(GetQuestionsQuery request, CancellationToken cancellationToken)
+    {
+        var questions = await _repository.Question.GetQuestions(request.SearchTerm, cancellationToken);
+
+        return questions;
+    }
+}
---
+++ Application/Core/Question/Queries/GetQuestions/GetQuestionsQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using Shared.DTOs.Question;
+
+namespace Application.Core.Question.Queries.GetQuestions;
+public sealed record GetQuestionsQuery(string SearchTerm) : IRequest<IEnumerable<QuestionWithIdDTO>>;
