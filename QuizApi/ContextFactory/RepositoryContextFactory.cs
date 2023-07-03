480ec29120dac9950c4ae73d5dc61c8002146806
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-03 14:47:14 +0200

    Cleaning up ContextFactory

--- QuizApi/ContextFactory/RepositoryContextFactory.cs
+++ QuizApi/ContextFactory/RepositoryContextFactory.cs
@@ -2,22 +2,20 @@
 using Microsoft.EntityFrameworkCore.Design;
 using Repository;
 
-namespace QuizApi.ContextFactory
+namespace QuizApi.ContextFactory;
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
 {
-    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    public RepositoryContext CreateDbContext(string[] args)
     {
-        public RepositoryContext CreateDbContext(string[] args)
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
 
-            var builder = new DbContextOptionsBuilder<RepositoryContext>()
-                .UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
-                b => b.MigrationsAssembly("QuizApi")).UseSnakeCaseNamingConvention();
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+            b => b.MigrationsAssembly("QuizApi")).UseSnakeCaseNamingConvention();
 
-            return new RepositoryContext(builder.Options);
-        }
+        return new RepositoryContext(builder.Options);
     }
 }
