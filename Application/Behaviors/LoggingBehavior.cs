9158d99e12a0d3f7a172a7310f30dba6bdde606a
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-04 13:56:45 +0200

    Adding logging behavior

---
+++ Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Contracts;
+using MediatR;
+using System.Diagnostics;
+
+namespace Application.Behaviors;
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+{
+    private readonly ILoggerManager _loggerManager;
+
+    public LoggingBehavior(ILoggerManager loggerManager)
+    {
+        _loggerManager = loggerManager;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+        var timeBeforeReuqest = DateTime.UtcNow;
+
+        _loggerManager.LogInfo($"Starting request {requestName},{timeBeforeReuqest}");
+
+        var timer = new Stopwatch();
+        timer.Start();
+        var result = await next();
+        timer.Stop();
+
+        var timeAfterRequest = DateTime.UtcNow;
+
+        _loggerManager.LogInfo($"Completed request {requestName}, {timeAfterRequest}, total request time:{timer.ElapsedMilliseconds}");
+
+        return result;
+    }
+}
