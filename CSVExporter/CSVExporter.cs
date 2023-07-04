f1ed188ec05ba4c5993ea0679cf597a30eea7857
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-04 16:59:21 +0200

    Adding export service and csv exporter project

---
+++ CSVExporter/CSVExporter.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using System.ComponentModel.Composition;
+
+namespace CSVExporter
+{
+    [Export(typeof(IExportService))]
+    public class DocFormatReader : IExportService
+    {
+        public string Format => ".csv";
+
+        public string Export(string path)
+        {
+            //var dc = DocumentCore.Load(path);
+            //var runList = dc.GetChildElements(true, ElementType.Run).Select(x => x.Content.ToString());
+            return "";
+        }
+    }
+}
---
+++ Contracts/IExportService.cs
@@ -0,0 +1,6 @@
+﻿namespace Contracts;
+public interface IExportService
+{
+    string Format { get; }
+    string Export(string path);
+}
