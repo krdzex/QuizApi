2ba9e805903b212668aa0fa0567d6d28875f9962
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-04 23:21:36 +0200

    Adding result to get quiz for export and handle if quiz is not found

--- Application/Core/Quiz/Queries/GetQuizForExport/GetQuizForExportHandler.cs
+++ Application/Core/Quiz/Queries/GetQuizForExport/GetQuizForExportHandler.cs
@@ -1,9 +1,10 @@
 ﻿using Contracts;
 using MediatR;
 using Shared.DTOs.Quiz;
+using Shared.Result;
 
 namespace Application.Core.Quiz.Queries.GetQuizForExport;
-internal sealed class GetQuizForExportHandler : IRequestHandler<GetQuizForExportQuery, QuizWithQuestionTextDTO>
+internal sealed class GetQuizForExportHandler : IRequestHandler<GetQuizForExportQuery, Result<QuizWithQuestionTextDTO>>
 {
     private readonly IRepositoryManager _repository;
 
@@ -12,13 +13,13 @@ internal sealed class GetQuizForExportHandler : IRequestHandler<GetQuizForExport
         _repository = repository;
     }
 
-    public async Task<QuizWithQuestionTextDTO> Handle(GetQuizForExportQuery request, CancellationToken cancellationToken)
+    public async Task<Result<QuizWithQuestionTextDTO>> Handle(GetQuizForExportQuery request, CancellationToken cancellationToken)
     {
         var quiz = await _repository.Quiz.GetQuizForExport(request.QuizId, cancellationToken);
 
         if (quiz is null)
         {
-
+            return Result<QuizWithQuestionTextDTO>.NotFound($"Quiz with id '{request.QuizId}' not found.");
         }
 
         return quiz;
--- Application/Core/Quiz/Queries/GetQuizForExport/GetQuizForExportQuery.cs
+++ Application/Core/Quiz/Queries/GetQuizForExport/GetQuizForExportQuery.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Shared.DTOs.Quiz;
+using Shared.Result;
 
 namespace Application.Core.Quiz.Queries.GetQuizForExport;
-public sealed record GetQuizForExportQuery(int QuizId) : IRequest<QuizWithQuestionTextDTO>;
+public sealed record GetQuizForExportQuery(int QuizId) : IRequest<Result<QuizWithQuestionTextDTO>>;
