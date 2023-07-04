7efa0116903a2bccda89ff9e724bc7112f678efb
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-04 23:18:30 +0200

    Adding our result to remove question from quiz. Handle failures as well

--- Application/Core/Quiz/Commands/RemoveQuestionFromQuiz/RemoveQuestionFromQuizCommand.cs
+++ Application/Core/Quiz/Commands/RemoveQuestionFromQuiz/RemoveQuestionFromQuizCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Shared.Result;
 
 namespace Application.Core.Quiz.Commands.RemoveQuestionFromQuiz;
-public sealed record RemoveQuestionFromQuizCommand(int QuizId, int QuestionId) : IRequest<Unit>;
+public sealed record RemoveQuestionFromQuizCommand(int QuizId, int QuestionId) : IRequest<Result>;
--- Application/Core/Quiz/Commands/RemoveQuestionFromQuiz/RemoveQuestionFromQuizHandler.cs
+++ Application/Core/Quiz/Commands/RemoveQuestionFromQuiz/RemoveQuestionFromQuizHandler.cs
@@ -1,8 +1,9 @@
 ﻿using Contracts;
 using MediatR;
+using Shared.Result;
 
 namespace Application.Core.Quiz.Commands.RemoveQuestionFromQuiz;
-internal sealed class RemoveQuestionFromQuizHandler : IRequestHandler<RemoveQuestionFromQuizCommand, Unit>
+internal sealed class RemoveQuestionFromQuizHandler : IRequestHandler<RemoveQuestionFromQuizCommand, Result>
 {
     private readonly IRepositoryManager _repository;
 
@@ -11,29 +12,29 @@ internal sealed class RemoveQuestionFromQuizHandler : IRequestHandler<RemoveQues
         _repository = repository;
     }
 
-    public async Task<Unit> Handle(RemoveQuestionFromQuizCommand request, CancellationToken cancellationToken)
+    public async Task<Result> Handle(RemoveQuestionFromQuizCommand request, CancellationToken cancellationToken)
     {
         var quizExist = await _repository.Quiz.QuizExists(request.QuizId, cancellationToken);
 
         if (!quizExist)
         {
-
+            return Result.NotFound($"Quiz with id '{request.QuizId}' not found.");
         }
 
         var questionExist = await _repository.Question.QuestionExists(request.QuestionId, cancellationToken);
 
         if (!questionExist)
         {
-
+            return Result.NotFound($"Question with id '{request.QuizId}' not found.");
         }
 
         var removeResult = await _repository.Quiz.RemoveQuestionFromQuiz(request.QuizId, request.QuestionId, cancellationToken);
 
         if (!removeResult)
         {
-
+            return Result.BadRequest($"Remove question from quiz is not successfully done. Chack that question exist in quiz in order to remove it");
         }
 
-        return Unit.Value;
+        return Result.Success();
     }
 }
