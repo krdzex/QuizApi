51d1ff7ada8b2b925e63e528c93a06d4711ecced
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-04 23:06:28 +0200

    Making my own api controller as abstraction

--- QuizApi.Presentation/Controllers/QuizController.cs
+++ QuizApi.Presentation/Controllers/QuizController.cs
@@ -1,115 +1,106 @@
-﻿using Application.Core.Quiz.Commands.CreateQuiz;
-using Application.Core.Quiz.Commands.DeleteQuiz;
-using Application.Core.Quiz.Commands.RemoveQuestionFromQuiz;
-using Application.Core.Quiz.Commands.UpdateQuizName;
-using Application.Core.Quiz.Queries.GetQuizForExport;
-using Application.Core.Quiz.Queries.GetQuizWithQuestions;
-using Application.Core.Quiz.Queries.GetQuizzes;
+﻿using Application.Core.Quiz.Queries.GetQuizWithQuestions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Shared.DTOs.Quiz;
-using System.Text;
+using QuizApi.Presentation.Infrastructure;
 
 namespace QuizApi.Presentation.Controllers;
 
-[ApiController]
 [Route("api/[controller]")]
-public class QuizController : ControllerBase
+public class QuizController : ApiController
 {
-    private readonly ISender _sender;
     private readonly ExporterProvider _exporterProvider;
 
     public QuizController(ISender sender)
+        : base(sender)
     {
-        _sender = sender;
         _exporterProvider = new ExporterProvider();
     }
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllQuizzes(CancellationToken cancellationToken)
-    {
-        var query = new GetQuizzesQuery();
+    //[HttpGet]
+    //public async Task<IActionResult> GetAllQuizzes(CancellationToken cancellationToken)
+    //{
+    //    var query = new GetQuizzesQuery();
 
-        var response = await _sender.Send(query, cancellationToken);
+    //    var response = await _sender.Send(query, cancellationToken);
 
-        return Ok(response);
-    }
+    //    return Ok(response);
+    //}
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetQuizById(int id, CancellationToken cancellationToken)
     {
         var query = new GetQuizWithQuestionsQuery(id);
 
-        var response = await _sender.Send(query, cancellationToken);
+        var response = await Sender.Send(query, cancellationToken);
 
-        return Ok(response);
+        return response.IsSuccess ? Ok(response.Value) : HandleFailure(response);
     }
 
-    [HttpPost]
-    public async Task<IActionResult> CreateQuiz([FromBody] QuizCreateDTO quizCreate, CancellationToken cancellationToken)
-    {
-        var command = new CreateQuizCommand(quizCreate);
+    //[HttpPost]
+    //public async Task<IActionResult> CreateQuiz([FromBody] QuizCreateDTO quizCreate, CancellationToken cancellationToken)
+    //{
+    //    var command = new CreateQuizCommand(quizCreate);
 
-        var response = await _sender.Send(command, cancellationToken);
+    //    var response = await _sender.Send(command, cancellationToken);
 
-        return NoContent();
-    }
+    //    return NoContent();
+    //}
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteQuiz(int id, CancellationToken cancellationToken)
-    {
-        var command = new DeleteQuizCommand(id);
+    //[HttpDelete("{id}")]
+    //public async Task<IActionResult> DeleteQuiz(int id, CancellationToken cancellationToken)
+    //{
+    //    var command = new DeleteQuizCommand(id);
 
-        var response = await _sender.Send(command, cancellationToken);
+    //    var response = await _sender.Send(command, cancellationToken);
 
-        return NoContent();
-    }
+    //    return NoContent();
+    //}
 
-    [HttpPut("{id}/name")]
-    public async Task<IActionResult> DeleteQuiz(int id, QuizNameUpdateDTO quizNameUpdate, CancellationToken cancellationToken)
-    {
-        var command = new UpdateQuizNameCommand(id, quizNameUpdate);
+    //[HttpPut("{id}/name")]
+    //public async Task<IActionResult> DeleteQuiz(int id, QuizNameUpdateDTO quizNameUpdate, CancellationToken cancellationToken)
+    //{
+    //    var command = new UpdateQuizNameCommand(id, quizNameUpdate);
 
-        var response = await _sender.Send(command, cancellationToken);
+    //    var response = await _sender.Send(command, cancellationToken);
 
-        return NoContent();
-    }
+    //    return NoContent();
+    //}
 
-    [HttpDelete("{quizId}/question/{questionId}")]
+    //[HttpDelete("{quizId}/question/{questionId}")]
 
-    public async Task<IActionResult> DeleteQuestionFromQuiz(int quizId, int questionId, CancellationToken cancellationToken)
-    {
-        var command = new RemoveQuestionFromQuizCommand(quizId, questionId);
+    //public async Task<IActionResult> DeleteQuestionFromQuiz(int quizId, int questionId, CancellationToken cancellationToken)
+    //{
+    //    var command = new RemoveQuestionFromQuizCommand(quizId, questionId);
 
-        var response = await _sender.Send(command, cancellationToken);
+    //    var response = await _sender.Send(command, cancellationToken);
 
-        return NoContent();
-    }
+    //    return NoContent();
+    //}
 
-    [HttpGet("exporter")]
-    public async Task<IActionResult> GetAllExporters()
-    {
-        var exporters = _exporterProvider.GetExporters();
+    //[HttpGet("exporter")]
+    //public async Task<IActionResult> GetAllExporters()
+    //{
+    //    var exporters = _exporterProvider.GetExporters();
 
-        return Ok(exporters);
-    }
+    //    return Ok(exporters);
+    //}
 
-    [HttpGet("{quizId}/exporter/{exportFormat}")]
-    public async Task<IActionResult> ExportQuiz(int quizId, string exportFormat, CancellationToken cancellationToken)
-    {
-        var query = new GetQuizForExportQuery(quizId);
+    //[HttpGet("{quizId}/exporter/{exportFormat}")]
+    //public async Task<IActionResult> ExportQuiz(int quizId, string exportFormat, CancellationToken cancellationToken)
+    //{
+    //    var query = new GetQuizForExportQuery(quizId);
 
-        var response = await _sender.Send(query, cancellationToken);
+    //    var response = await _sender.Send(query, cancellationToken);
 
-        var exporter = _exporterProvider.GetExporter(exportFormat);
+    //    var exporter = _exporterProvider.GetExporter(exportFormat);
 
-        if (exporter == null)
-        {
-            return BadRequest("Invalid export format");
-        }
+    //    if (exporter == null)
+    //    {
+    //        return BadRequest("Invalid export format");
+    //    }
 
-        var exportedData = exporter.ExportAsync(response);
+    //    var exportedData = exporter.ExportAsync(response);
 
-        return File(Encoding.UTF8.GetBytes(exportedData), "text/csv", $"{quizId}.csv");
-    }
+    //    return File(Encoding.UTF8.GetBytes(exportedData), "text/csv", $"{quizId}.csv");
+    //}
 }
---
+++ QuizApi.Presentation/Infrastructure/ApiController.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Result;
+
+namespace QuizApi.Presentation.Infrastructure;
+
+[ApiController]
+public abstract class ApiController : ControllerBase
+{
+    protected readonly ISender Sender;
+
+    protected ApiController(ISender sender) => Sender = sender;
+
+    protected IActionResult HandleFailure(Result result)
+    {
+        switch (result)
+        {
+            case { IsSuccess: true }:
+                throw new InvalidOperationException();
+            default:
+                return CreateProblemResult(result);
+        }
+    }
+
+    protected IActionResult HandleFailure<T>(Result<T> result)
+    {
+        switch (result)
+        {
+            case { IsSuccess: true }:
+                throw new InvalidOperationException();
+            default:
+                return CreateProblemResult(result);
+        }
+    }
+
+    private IActionResult CreateProblemResult(ResultBase result)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Type = $"https://httpstatuses.com/{(int)result.StatusCode}",
+            Title = "",
+            Status = (int)result.StatusCode,
+            Detail = result.ErrorMessage?.Message,
+        };
+
+        if (result.Errors?.Length > 0)
+        {
+            problemDetails.Extensions[nameof(result.Errors)] = result.Errors;
+        }
+
+        return StatusCode((int)result.StatusCode, problemDetails);
+    }
+}
