14c61ae4e4e690c4f2f5ad90590aaee52a754629
Krsto Kostic <kostic.krsto@gmail.com>
2023-07-05 02:44:01 +0200

    Update quiz name validator

---
+++ Application/Validators/UpdateQuizNameCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.Core.Quiz.Commands.UpdateQuizName;
+using FluentValidation;
+
+namespace Application.Validators;
+public sealed class UpdateQuizNameCommandValidator :
+    AbstractValidator<UpdateQuizNameCommand>
+{
+    public UpdateQuizNameCommandValidator()
+    {
+        RuleFor(q => q.QuizNameUpdate.Name)
+            .NotEmpty()
+                .WithMessage("Quiz name is required")
+            .MaximumLength(100)
+                .WithMessage("Max number of characters is 100");
+
+    }
+}
